ab75c03e4d785806e9988c80f44ff002e25c32e1
kasemsan00 <kasemsan.cho@gmail.com>
2023-04-14 21:59:27 +0700

    dev

---
+++ src/interface/data.tsx
@@ -0,0 +1,33 @@
+export interface IDataArray {
+  data: IData[];
+}
+export interface IData {
+  id: string;
+  company: string;
+  logo: string;
+  new: boolean;
+  featured: boolean;
+  position: string;
+  role: string;
+  level: string;
+  postedAt: string;
+  contract: string;
+  location: string;
+  languages: string[];
+  tools: string[];
+}
+
+// "id": 1,
+// 	"company": "Photosnap",
+// 	"logo": "./images/photosnap.svg",
+// 	"new": true,
+// 	"featured": true,
+// 	"position": "Senior Frontend Developer",
+// 	"role": "Frontend",
+// 	"level": "Senior",
+// 	"postedAt": "1d ago",
+// 	"contract": "Full Time",
+// 	"location": "USA Only",
+// 	"languages": ["HTML", "CSS", "JavaScript"],
+// 	"tools": []
+//
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,24 @@
-import Card from "@/components/card/Card";
+import { IData } from "@/interface/data";
+import dataJSON from "../data.json";
+import React, { useEffect } from "react";
 
 export default function Home() {
+  const [data, setData] = React.useState<IData[]>([]);
+  useEffect(() => {
+    setData(dataJSON);
+  }, []);
+  // React.useEffect(() => {
+  //   fetch("../data.json")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setData(data);
+  //     });
+  // }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-green-100">
-	    <Card/>
+      {/*{data.map((item) => (*/}
+      {/*  <div key={item.id}>{item.company}</div>*/}
+      {/*))}*/}
     </main>
-  )
+  );
 }
